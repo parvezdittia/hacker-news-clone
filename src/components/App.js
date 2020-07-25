7f6f9c7e0a3ddd926fe78097fedfd086dd8a12db
unknown <parvezdittia@gmail.com>
2020-07-26 00:26:44 +0530

    Simple Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,15 @@
 import React, { Component } from "react";
+import Header from "./Header/Header";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      counter: 0,
-    };
-    this.incrementCounter = this.incrementCounter.bind(this);
-  }
-
-  incrementCounter() {
-    let counter = this.state.counter;
-    this.setState({
-      counter: counter + 1,
-    });
   }
 
   render() {
     return (
       <>
-        <p>React Hydration</p>
-        <p>Counter: {this.state.counter}</p>
-        <button onClick={this.incrementCounter}> Counter Click</button>
+        <Header></Header>
       </>
     );
   }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Header(props) {
+  return <header>HackerNews</header>;
+}
+
+export default Header;
