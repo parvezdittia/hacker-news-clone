78ef9c01f94b3db7c041a0fd21a415c7e17301d4
unknown <parvezdittia@gmail.com>
2020-07-25 19:04:32 +0530

    CSR Code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 
-function App() {
-  return <p>React SSR</p>;
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    };
+    this.incrementCounter = this.incrementCounter.bind(this);
+  }
+
+  incrementCounter() {
+    let counter = this.state.counter;
+    this.setState({
+      counter: counter + 1,
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <p>React Hydration</p>
+        <p>Counter: {this.state.counter}</p>
+        <button onClick={this.incrementCounter}> Counter Click</button>
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+console.log(path.resolve(__dirname, "./functions/src/static"));
+
+module.exports = {
+  entry: "./src/hydrate.js",
+
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./functions/static"),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+};
